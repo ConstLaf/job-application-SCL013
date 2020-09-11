29d26f579ef12bf469a157eb18a68a8f17bd1507
ConstLaf <developer.constance@gmail.com>
2020-09-11 15:52:49 -0300

    Fix navbar toggle

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { HeaderComponent } from './component/header/header.component';
 import { FooterComponent } from './component/footer/footer.component';
 import { HomeComponent } from './component/home/home.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ProjectsComponent } from './component/projects/projects.component';
 import { AboutComponent } from './component/about/about.component';
 import { SkillsComponent } from './component/skills/skills.component';
@@ -28,7 +29,8 @@ import { ContactMeComponent } from './component/contact-me/contact-me.component'
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    NgbCollapseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
